477c1035674d89aaae3424948555fc984ba0682b
Dadaskis <37717199+Dadaskis@users.noreply.github.com>
2018-05-14 00:01:33 +0300

    Add files via upload
    Build 2

---
+++ ChunkManager.lua
@@ -0,0 +1,31 @@
+ChunkManager = {}
+
+ChunkManager.chunks = {}
+
+ChunkManager.previousPlayerPos = {
+    X = -1337,
+    Z =  1337
+}
+
+ChunkManager.loadChunks = function(this)
+    local cameraX = math.floor(Engine.getCurrentCameraPosition().X / 16)
+    local cameraZ = math.floor(Engine.getCurrentCameraPosition().Z / 16)
+    if(cameraX ~= this.previousPlayerPos.X or cameraZ ~= this.previousPlayerPos.Z) then
+        this.previousPlayerPos.X = cameraX
+        this.previousPlayerPos.Z = cameraZ
+        BlockManager:clear()
+        for XChunk = cameraX - CHUNK_RENDER_DISTANCE, cameraX + CHUNK_RENDER_DISTANCE do
+            if this.chunks[XChunk] == nil then
+                this.chunks[XChunk] = {}
+            end
+            for ZChunk = cameraZ - CHUNK_RENDER_DISTANCE, cameraZ + CHUNK_RENDER_DISTANCE do
+                if this.chunks[XChunk][ZChunk] == nil then
+                    this.chunks[XChunk][ZChunk] = Chunk(XChunk, ZChunk)
+                    this.chunks[XChunk][ZChunk]:setHeightMap(ChunkGenerator:generateHeightMap(XChunk, ZChunk))
+                end
+                this.chunks[XChunk][ZChunk]:updateModels()
+            end
+        end
+    end
+    BlockManager:draw()
+end
---
+++ Prepare.lua
@@ -0,0 +1,7 @@
+print("Preparing...")
+
+Engine.setCurrentCameraFOV(90)
+Engine.setCurrentCameraSpeed(0.05)
+Engine.setCurrentCameraPosition(0, 60, 0)
+
+print("Prepare is done")
